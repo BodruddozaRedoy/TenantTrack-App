e5ee173d99dfbec1d63271053830a9cc16758464
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-11-15 10:28:04 +0600

    create 5 files

---
+++ app/(agent)/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AgentLayout = () => {
+  return (
+    <View>
+      <Text>AgentLayout</Text>
+    </View>
+  )
+}
+
+export default AgentLayout
