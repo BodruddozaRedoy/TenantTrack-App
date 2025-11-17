7a2e548156e61d873962289f2ee0ec72e1c403e2
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-11-17 10:30:23 +0600

    update profile.tsx and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
         text: "#000000",
         primary: "#000000",
         secondary: "#3A3A3A",
-        card: "#FFFFFF",
+        card: "#F5F5F5",
 
         // Dark
         backgroundDark: "#000000",
